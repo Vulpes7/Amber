4cb2392c06fc5f4afc305eef760ecfe7fd8e5cf5
Vulpes7 <sjohannes7@gmail.com>
2026-03-19 23:34:14 +0530

    Create About Us page with Next.js Metadata

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'About Us',
+  description: 'Learn more about our team and values.'
+};
+
+export default function AboutPage() {
+  return <div>About Us Page Content Here</div>;
+}
